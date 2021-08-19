7ed0735179c6d22cf3bd067dd671d73868795c62
Mary Rodriguez <31219221+mrodriguezt@users.noreply.github.com>
2021-08-19 15:55:37 -0500

    SZK-29 Changes token inconcert

--- app/Services/TicketInconcertClass.php
+++ app/Services/TicketInconcertClass.php
@@ -1,9 +1,6 @@
 <?php
 namespace App\Services;
 
-use App\Models\Tickets;
-use App\Models\TicketsCstm;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Auth;
 
