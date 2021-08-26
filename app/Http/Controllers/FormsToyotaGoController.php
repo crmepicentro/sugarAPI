1685e69341a036a1d0b01b45495fd76f3b2acdb1
27andrew <andres.g.alejandro@hotmail.com>
2021-08-26 12:00:43 -0500

    TOYOTA-76 FIX modelos

--- app/Http/Controllers/FormsToyotaGoController.php
+++ app/Http/Controllers/FormsToyotaGoController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Contacts;
-use App\Models\Destinations;
-use App\Models\DestinationSuggestions;
 use App\Models\WsToyotaGo;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
