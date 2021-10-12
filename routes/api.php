811004b48a37baad1f1f74bc262c0c9256d9efd2
Cristian Cazares <dev.ccazares@gmail.com>
2021-10-12 12:14:36 -0500

    Revert "data send back"
    This reverts commit e95e4e0db8e18c8213fe04219e190b780d8411a6.

--- routes/api.php
+++ routes/api.php
@@ -53,7 +53,6 @@ $api->version('v1', ['middleware' => ['api.throttle', 'auth:sanctum'], 'limit' =
     $api->put('coupons/', 'App\Http\Controllers\CouponsController@update');
     $api->post('coupons/validate', 'App\Http\Controllers\CouponsController@validateCoupon');
     $api->post('c2cOmnichannel', 'App\Http\Controllers\OmnichannelController@sendToOmnichannel');
-    $api->post('prueba', 'App\Http\Controllers\OmnichannelController@prueba');
 });
 
 $api->version('v1', function ($api) {
