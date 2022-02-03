b0e334961e47b47cac1cedb105575d45c3caaa07
byronnp <byron_np@hotmail.com>
2022-02-02 20:18:06 -0500

    validaciones de casabaca

--- app/Console/Commands/setMissedMeetings.php
+++ app/Console/Commands/setMissedMeetings.php
@@ -49,7 +49,7 @@ class setMissedMeetings extends Command
         $user_auth = Auth::user();
 
         if(!$user_auth){
-          $user = Auth::loginUsingId(31);
+          $user = Auth::loginUsingId(1);
 
           if($user->fuente !== 'tests_source'){
             $user->connection = 'prod';
