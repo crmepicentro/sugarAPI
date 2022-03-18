ffb846d9f908ad9947fbc49dae95a7b759cf345e
byronnp <byron_np@hotmail.com>
2022-03-18 12:37:05 -0500

    validacion y actualizacion de meetings duplicados clicktocall

--- app/Models/Meetings.php
+++ app/Models/Meetings.php
@@ -141,7 +141,6 @@ class Meetings extends Model
         foreach($duplicados as $duplicado){
             \DB::connection(get_connection())->table('meetings')->where('id', $duplicado->codigo)->update(['status'=>'Not Held']);
         }
-
         return $duplicados;
 
     }
