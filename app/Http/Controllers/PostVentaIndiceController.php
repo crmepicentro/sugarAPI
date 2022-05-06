2748c0be0290990c4005930f60da7ed20db97221
Wagner Cadena <wcadena@casabaca.com>
2022-05-06 11:54:01 -0500

    Se coloca opcion de Lista grande

--- app/Http/Controllers/PostVentaIndiceController.php
+++ app/Http/Controllers/PostVentaIndiceController.php
@@ -15,26 +15,57 @@ class PostVentaIndiceController extends Controller
     public function __invoke(Request $request)
     {
         $data_list = "SELECT
+  pvt_propietarios.id,
   pvt_propietarios.nombre_propietario,
   pvt_propietarios.email_propietario,
+  pvt_propietarios.email_propietario_2,
   pvt_propietarios.telefono_domicilio,
-  MIN(pvt_gestion_agendado_detalle_op.created_at) AS Min_created_at_masterGestion,
-  MIN(pvt_gestion_agendados.created_at) AS Min_created_atDetalleGestion,
-  pvt_propietarios.id,
-  COUNT(pvt_autos.id) AS Count_id_autos
-FROM pvt_gestion_agendado_detalle_op pvt_gestion_agendado_detalle_op
-  INNER JOIN pvt_gestion_agendados pvt_gestion_agendados
-    ON pvt_gestion_agendado_detalle_op.gestion_agendado_id = pvt_gestion_agendados.id
-  RIGHT OUTER JOIN pvt_detalle_gestion_oportunidades pvt_detalle_gestion_oportunidades
-    ON pvt_gestion_agendado_detalle_op.detalle_gestion_oportunidad_id = pvt_detalle_gestion_oportunidades.id
-  INNER JOIN pvt_autos pvt_autos
-    ON pvt_detalle_gestion_oportunidades.auto_id = pvt_autos.id
+  pvt_propietarios.telefono_trabajo,
+  pvt_propietarios.telefono_celular,
+  pvt_detalle_gestion_oportunidades.cita_fecha,
+  pvt_detalle_gestion_oportunidades.agendado_fecha,
+  pvt_detalle_gestion_oportunidades.ganado_fecha,
+  pvt_detalle_gestion_oportunidades.gestion_tipo,
+  pvt_detalle_gestion_oportunidades.s3s_codigo_seguimiento,
+  MIN(IFNULL(pvt_gestion_agendado_detalle_op.created_at, '')) AS primer_gestion_v2,
+  (SELECT
+      interno1.tipo_gestion
+    FROM pvt_gestion_agendado_detalle_op interno1
+    WHERE interno1.detalle_gestion_oportunidad_id IN (SELECT
+        oportunidades_inter_ca1.id
+      FROM pvt_detalle_gestion_oportunidades oportunidades_inter_ca1
+        INNER JOIN pvt_autos auto_inter_ca1
+          ON oportunidades_inter_ca1.auto_id = auto_inter_ca1.id
+      WHERE oportunidades_inter_ca1.agendado_fecha = pvt_detalle_gestion_oportunidades.agendado_fecha
+      AND auto_inter_ca1.id = pvt_autos.id
+      AND oportunidades_inter_ca1.ganado_fecha = pvt_detalle_gestion_oportunidades.ganado_fecha
+      AND oportunidades_inter_ca1.gestion_tipo = pvt_detalle_gestion_oportunidades.gestion_tipo
+      AND oportunidades_inter_ca1.cita_fecha = pvt_detalle_gestion_oportunidades.cita_fecha
+      AND oportunidades_inter_ca1.s3s_codigo_seguimiento = pvt_detalle_gestion_oportunidades.s3s_codigo_seguimiento)
+    ORDER BY interno1.created_at DESC
+    LIMIT 1) AS primer_gestion_estado_v2,
+  pvt_autos.id
+FROM pvt_autos pvt_autos
   INNER JOIN pvt_propietarios pvt_propietarios
     ON pvt_autos.propietario_id = pvt_propietarios.id
-GROUP BY pvt_propietarios.nombre_propietario,
+  INNER JOIN pvt_detalle_gestion_oportunidades pvt_detalle_gestion_oportunidades
+    ON pvt_detalle_gestion_oportunidades.auto_id = pvt_autos.id
+  LEFT OUTER JOIN pvt_gestion_agendado_detalle_op pvt_gestion_agendado_detalle_op
+    ON pvt_gestion_agendado_detalle_op.detalle_gestion_oportunidad_id = pvt_detalle_gestion_oportunidades.id
+GROUP BY pvt_propietarios.id,
+         pvt_propietarios.nombre_propietario,
          pvt_propietarios.email_propietario,
+         pvt_propietarios.email_propietario_2,
          pvt_propietarios.telefono_domicilio,
-         pvt_propietarios.id";
+         pvt_propietarios.telefono_trabajo,
+         pvt_propietarios.telefono_celular,
+         pvt_detalle_gestion_oportunidades.agendado_fecha,
+         pvt_detalle_gestion_oportunidades.ganado_fecha,
+         pvt_detalle_gestion_oportunidades.gestion_tipo,
+         pvt_detalle_gestion_oportunidades.cita_fecha,
+         pvt_detalle_gestion_oportunidades.s3s_codigo_seguimiento,
+         pvt_autos.id
+ORDER BY FIELD(pvt_detalle_gestion_oportunidades.gestion_tipo, 'recordatorio', 'cita', 'nuevo', 'perdido'), MIN(pvt_detalle_gestion_oportunidades.gestion_fecha) DESC limit 100";
 
         $lista_oportunidades = \DB::select($data_list, ['N']);
 
--- resources/views/postventas/indexList.blade.php
+++ resources/views/postventas/indexList.blade.php
@@ -18,21 +18,22 @@
             <table class="table table-bordered table-striped table-vcenter js-dataTable-full table-sm">
                 <thead>
                 <tr>
-                    <th class="text-center" style="width: 80px;">#</th>
+                    <th class="text-center">#</th>
                     <th>Cliente</th>
                     <th class="d-none d-sm-table-cell" style="width: 30%;">Mail</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">Teléfono</th>
-                    <th class="d-none d-sm-table-cell" style="width: 15%;">Gestiones x Auto</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">RFM</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">R</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">F</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">M</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">VHC</th>
-                    <th class="d-none d-sm-table-cell" style="width: 15%;">1er Gestión Estado</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">1er Gestión Fecha</th>
+<!--                    <th class="d-none d-sm-table-cell" style="width: 15%;">1er Gestión Estado</th>-->
                     <th style="width: 15%;"><i class="fa fa-play"></i></th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">Gestión Futura Fecha</th>
-                    <th class="d-none d-sm-table-cell" style="width: 15%;">Gestión Futura Tipo</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">Cita Fecha</th>
                     <th class="d-none d-sm-table-cell" style="width: 15%;">Gestión Futura Estado</th>
+                    <th class="d-none d-sm-table-cell" style="width: 15%;">Gestión Futura Or</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -43,25 +44,28 @@
                             <a href="#">{{ $lista_oportunidade->nombre_propietario }}</a>
                         </td>
                         <td class="d-none d-sm-table-cell">
-                            {{ $lista_oportunidade->email_propietario }}
+                            {{ $lista_oportunidade->email_propietario }} {{ ($lista_oportunidade->email_propietario_2 != '' )?" | ".$lista_oportunidade->email_propietario_2:"" }}
                         </td>
                         <td class="d-none d-sm-table-cell">
-                            {{ $lista_oportunidade->telefono_domicilio ?? $lista_oportunidade->telefono_trabajo ?? $lista_oportunidade->telefono_celular ?? 'No tiene' }}
+                            {{ $lista_oportunidade->telefono_domicilio ?? $lista_oportunidade->telefono_domicilio ?? $lista_oportunidade->telefono_celular ?? 'No tiene' }}
+                            {{ ($lista_oportunidade->telefono_trabajo != '' )?" | ".$lista_oportunidade->telefono_trabajo:"" }}
+                            {{ ($lista_oportunidade->telefono_celular != '' )?" | ".$lista_oportunidade->telefono_celular:"" }}
                         </td>
-                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->Count_id_autos }}</td>
                         <td class="d-none d-sm-table-cell">{{ 'RFM' }}</td>
                         <td class="d-none d-sm-table-cell">{{ 'R' }}</td>
                         <td class="d-none d-sm-table-cell">{{ 'F' }}</td>
                         <td class="d-none d-sm-table-cell">{{ 'M' }}</td>
                         <td class="d-none d-sm-table-cell">{{ 'VHC' }}</td>
-                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->Min_created_at_masterGestion }}</td>
+                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->primer_gestion_v2 }}</td>
+<!--                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->primer_gestion_estado_v2 }}</td>-->
                         <td><a href="{{ route('postventa.edita', $lista_oportunidade->id) }}" >
                                 <i class="fa fa-play"></i>
                             </a>
                         </td>
-                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->Min_created_at_masterGestion }}</td>
-                        <td>{{ $lista_oportunidade->Min_created_at_masterGestion }}</td>
-                        <td>{{ $lista_oportunidade->Min_created_at_masterGestion }}</td>
+                        <td class="d-none d-sm-table-cell">{{ $lista_oportunidade->agendado_fecha }}</td>
+                        <td>{{ __($lista_oportunidade->cita_fecha) }}</td>
+                        <td>{{ __($lista_oportunidade->gestion_tipo) }}</td>
+                        <td>{{ $lista_oportunidade->s3s_codigo_seguimiento }}</td>
                     </tr>
                 @endforeach
 
