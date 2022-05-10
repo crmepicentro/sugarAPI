6453b0bacce33f136e150bf3869853bf07417088
Wagner Cadena <wcadena@casabaca.com>
2022-05-10 12:36:13 -0500

    Se aumenta opcion de presentacion de datos a s3s con json

--- resources/views/postventas/gestion/simula_s3s.blade.php
+++ resources/views/postventas/gestion/simula_s3s.blade.php
@@ -3,6 +3,10 @@
     <pre>
     {{ print_r($gestionAgendado->citas3s,true) }}
     </pre>
+    <hr>
+    <pre>
+    {{ json_encode($gestionAgendado->citas3s) }}
+    </pre>
 </code>
 
 <a href="{{ route('postventa.s3spostdatacorerespuesta',['codigo_seguimiento' =>$gestionAgendado->codigo_seguimiento,'respuesta'=> Str::random(10)]) }}" class="btn btn-primary">Respuesta de S3S</a>
